6aa3821f0baf1da30b0c999b6490ad7f98b72b12
Obtuse <theobtuseautodidact@gmail.com>
2017-08-03 23:12:20 -0700

    Initial setup

---
+++ src/main/java/com/launchcode/helloagainspring/HelloAgainSpringApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.helloagainspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAgainSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloAgainSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/launchcode/helloagainspring/HelloAgainSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.launchcode.helloagainspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloAgainSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
